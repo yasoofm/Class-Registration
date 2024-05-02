1a70ac72c4a298b1cb243058e1672eae763ea665
KFH <KFH@LAPTOP-83314>
2024-05-02 09:37:58 +0300

    added controllers

---
+++ ClassRegistrationBack/Controllers/AdminController.cs
@@ -0,0 +1,41 @@
+﻿using ClassRegistrationBack.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClassRegistrationBack.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        private readonly ILogger<AdminController> _logger;
+        private readonly ClassContext _context;
+
+        public AdminController(ILogger<AdminController> logger, ClassContext dbContext)
+        {
+            _logger = logger;
+            _context = dbContext;
+        }
+
+
+        [HttpPost("add-gym")]
+        [ProducesResponseType(typeof(IActionResult), 201)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult AddGym(AddGymRequset addGymRequest)
+        {
+            try
+            {
+                var gym = new Gym()
+                {
+                    Address = addGymRequest.Address,
+                    Name = addGymRequest.Name
+                };
+                _context.gyms.Add(gym);
+                _context.SaveChanges();
+                return Created();
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, ex);
+            }
+        }
+    }
+}
---
+++ ClassRegistrationBack/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using ClassRegistrationBack.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClassRegistrationBack.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ILogger<UserController> _logger;
+        private readonly ClassContext _context;
+
+        public UserController(ILogger<UserController> logger, ClassContext dbContext)
+        {
+            _logger = logger;
+            _context = dbContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
