6b7228bc5c06220db80ad54a71153341ecd11fd7
KFH <KFH@LAPTOP-83314>
2024-05-05 11:31:39 +0300

    done with all adds

---
+++ ClassRegistrationBack/Model/AddInstructorRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassRegistrationBack.Model
+{
+    public class AddInstructorRequest
+    {
+        public int PhoneNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ ClassRegistrationBack/Model/AddSectionRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace ClassRegistrationBack.Model
+{
+    public class AddSectionRequest
+    {
+        public TimeOnly Duration { get; set; }
+        public DateTime Time { get; set; }
+        public string SectionType { get; set; }
+        public int Capacity { get; set; }
+        public InstructorResponse? Instructor { get; set; }
+    }
+}
